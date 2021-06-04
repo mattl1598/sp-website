865574db1c31754def6208bb885c766e783fda1f
mattl1598 <mattl1598@gmail.com>
2021-06-04 23:47:45 +0100

    updating files for server deployment configured run.py for server updated requirements.txt for server
    Signed-off-by: mattl1598 <mattl1598@gmail.com>

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from webapp import app, routes, socketio
+import socket
 
 if __name__ == "__main__":
-	socketio.run(app, debug=True)
+	hostname = socket.gethostname()
+	if hostname == "BAIN":
+		socketio.run(app, host='0.0.0.0')
+	else:
+		socketio.run(app, debug=True)
